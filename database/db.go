ab9abb090ebd5a716b16b88d47c22392a72f6d0e
Fatima <faatimaashaarif@gmail.com>
2025-11-09 14:07:21 +0000

    fixed database and environment variables

--- database/db.go
+++ database/db.go
@@ -1,28 +1,34 @@
 package database
 
 import (
+	"log"
+	"os"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
 	"todolistapi/models"
 )
 
 var Db *gorm.DB
 
 func InitDB() {
-	// open the database
-	connStr := "user=postgres host=localhost password=password dbname=todolistapi port=5432 sslmode=disable"
+	// Get database URL from environment variable
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
 
 	var err error
+	// Open database connection
 	Db, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to connect to database: ", err)
 	}
-	
+
+	// Auto migrate tables
 	err = Db.AutoMigrate(&models.User{}, &models.TodoItem{})
 	if err != nil {
-		log.Fatal("error migrating table", err)
+		log.Fatal("Error migrating table: ", err)
 	}
+	
+	log.Println("Database connected and migrated successfully")
 }
-
-
